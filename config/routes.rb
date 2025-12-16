de481d865536f2299f2e1ef2fd07329e210e3075
Moses Ademokun <mazetechsolution@gmail.com>
2025-12-16 22:41:30 +0000

    Fix priorities views directory typo and routes keyword arguments

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   resources :priorities
 
   # User profile routes
-  resource :user, only: [], path: "users", controller: "users", path_names: { new: "signup" }
+  resource(:user, path: "users", controller: "users", path_names: { new: "signup" }, only: [])
 
   # Health check endpoint for monitoring (returns 200 if app is running)
   get "up" => "rails/health#show", as: :rails_health_check
